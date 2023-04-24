b6d47c8267ecf77a48743d4527cc776bc0d7696c
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2023-04-24 18:58:06 +0300

    Use HttpClient with IHttpClientFactory

---
+++ ReverseProxySln/ReverseProxyApi/Middlewares/LoadBalancerMiddleware.cs
@@ -0,0 +1,47 @@
+﻿namespace ReverseProxyApi.Middlewares
+{
+    public class LoadBalancerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IHttpClientFactory _clientFactory;
+
+        private static List<Uri> _serverUris;
+        private static int _currentServerIndex = 0;
+
+        public LoadBalancerMiddleware(RequestDelegate next, IHttpClientFactory clientFactory)
+        {
+            _next = next;
+            _clientFactory = clientFactory;
+
+            _serverUris = new List<Uri>
+            {
+                new Uri("https://www.microsoft.com/"),
+                new Uri("https://www.apple.com/"),
+                new Uri("https://www.google.com/")
+            };
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var serverUri = GetNextServerUri();
+            var requestUri = new Uri(serverUri, context.Request.Path);
+            var httpClient = _clientFactory.CreateClient("CustomHttpClient");
+
+            Console.WriteLine($"Called {requestUri}");
+
+            var responseMessage = await httpClient.GetAsync(requestUri);
+
+            context.Response.StatusCode = (int)responseMessage.StatusCode;
+            var responseContent = await responseMessage.Content.ReadAsByteArrayAsync();
+            await context.Response.Body.WriteAsync(responseContent, 0, responseContent.Length);
+        }
+
+        private static Uri GetNextServerUri()
+        {
+            var serverUri = _serverUris[_currentServerIndex];
+            _currentServerIndex = (_currentServerIndex + 1) % _serverUris.Count;
+            
+            return serverUri;
+        }
+    }
+}
---
+++ ReverseProxySln/ReverseProxyApi/Program.cs
@@ -0,0 +1,51 @@
+
+using ReverseProxyApi.Middlewares;
+using System.Net.Http.Headers;
+
+namespace ReverseProxyApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddHttpClient("CustomHttpClient", client =>
+            {
+                // Set a timeout for requests
+                // https://learn.microsoft.com/en-us/troubleshoot/azure/app-service/web-apps-performance-faqs#why-does-my-request-time-out-after-230-seconds
+                client.Timeout = TimeSpan.FromSeconds(230);
+            })
+            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+            {
+                //AllowAutoRedirect = true,
+                // Configure the HttpClientHandler, e.g., enable automatic decompression for better performance
+                //AutomaticDecompression = System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate | System.Net.DecompressionMethods.Brotli,
+            })
+            .SetHandlerLifetime(Timeout.InfiniteTimeSpan); 
+            //.AddPolicyHandler(/* Add a Polly policy for retries, timeouts, or other policies if needed */);
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseMiddleware<LoadBalancerMiddleware>(); // Register the Load Balancer Middleware
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
