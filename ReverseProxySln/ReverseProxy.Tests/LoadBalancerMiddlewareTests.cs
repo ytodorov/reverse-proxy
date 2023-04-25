2f2bd5d490423cfe9fddf05b0c57819db400cae3
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2023-04-25 22:54:47 +0300

    Implement LoadBalancerMiddlewareTests.

--- ReverseProxySln/ReverseProxy.Tests/LoadBalancerMiddlewareTests.cs
+++ ReverseProxySln/ReverseProxy.Tests/LoadBalancerMiddlewareTests.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
@@ -16,10 +19,49 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
+using ReverseProxy.Core.Extensions;
+
 namespace ReverseProxy.Tests
 {
     public class LoadBalancerMiddlewareTests
     {
-        
+        [Fact]
+        public async Task RoundRobin_Should_Be_Different()
+        {
+            using var server = CreateTestServer();
+            var client = server.CreateClient();
+
+            var response = await client.GetAsync("/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            var response2 = await client.GetAsync("/");
+            var content2 = await response2.Content.ReadAsStringAsync();
+
+            var response3 = await client.GetAsync("/");
+            var content3 = await response3.Content.ReadAsStringAsync();
+
+            Assert.NotEqual(content, content2);
+            Assert.NotEqual(content, content3);
+            Assert.NotEqual(content2, content3);
+
+
+        }
+
+        private TestServer CreateTestServer()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .ConfigureAppConfiguration(s => s.AddJsonFile("appsettings.json"))
+                .ConfigureServices(services => services.AddLoadBalancer())
+                .Configure(app =>
+                {
+                    app.UseMiddleware<LoadBalancerMiddleware>();
+                    app.Run(async context =>
+                    {
+                        await context.Response.WriteAsync("Test response");
+                    });
+                });
+
+            return new TestServer(webHostBuilder);
+        }
     }
 }
