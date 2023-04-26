2c0a131388e8e8a68a4dac543d67b5c62c975d89
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2023-04-26 19:14:56 +0300

    Implement simple RateLimiter to mitigate DDOS attacks.

--- ReverseProxySln/ReverseProxy.MinimalApi/Program.cs
+++ ReverseProxySln/ReverseProxy.MinimalApi/Program.cs
@@ -2,6 +2,7 @@ using ReverseProxy.Core.Classes;
 using ReverseProxy.Core.Interfaces;
 using ReverseProxy.Core.Middlewares;
 using ReverseProxy.Core.Extensions;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
--- ReverseProxySln/ReverseProxyApi/Program.cs
+++ ReverseProxySln/ReverseProxyApi/Program.cs
@@ -1,5 +1,7 @@
 using ReverseProxy.Core.Middlewares;
 using ReverseProxy.Core.Extensions;
+using System.Threading.RateLimiting;
+using System.Globalization;
 
 namespace ReverseProxyApi
 {
@@ -9,10 +11,38 @@ namespace ReverseProxyApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddRateLimiter(options =>
+            {
+                options.OnRejected = (context, ct) =>
+                {
+                    if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                    {
+                        context.HttpContext.Response.Headers.RetryAfter =
+                            ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+                    }
+
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                    context.HttpContext.Response.WriteAsync("Too many requests. Please try again later.");
+
+                    return new ValueTask();
+                };
+
+                options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+                {
+                    return RateLimitPartition.GetFixedWindowLimiter(partitionKey: httpContext.Request.Headers.Host.ToString(),
+                        factory: _ => new FixedWindowRateLimiterOptions
+                        {
+                            PermitLimit = 10,
+                            Window = TimeSpan.FromSeconds(10)
+                        });
+                });
+            });
+
             builder.Services.AddLoadBalancer();
 
             var app = builder.Build();
 
+            app.UseRateLimiter();
             app.UseMiddleware<LoadBalancerMiddleware>();
 
             app.Run();
