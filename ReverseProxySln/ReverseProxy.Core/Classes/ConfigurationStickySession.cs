b0bbf3b7f2b729fc886dba7aad83e66a8dc71556
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2023-04-27 19:42:57 +0300

    Implement ConfigurationStickySession that reads from config

---
+++ ReverseProxySln/ReverseProxy.Core/Classes/ConfigurationStickySession.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using ReverseProxy.Core.Extensions;
+using ReverseProxy.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseProxy.Core.Classes
+{
+    public class ConfigurationStickySession : IStickySession
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationStickySession(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+       
+        public bool IsStickySessionEnabled()
+        {
+            var stickySessionSection = _configuration.GetSection("LoadBalancer:EnableStickySession");
+            if (stickySessionSection == null)
+            {
+                return false;
+            }
+            
+            var isStickySessionEnabled = stickySessionSection.Get<bool>();
+            return isStickySessionEnabled;
+
+        }
+    }
+}
